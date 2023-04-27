718f023e910b86ecc0e1a66d5018de96ee77368f
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2023-04-27 18:32:01 -0400

    Organized code for estimation and calculated predicted prices

--- 5_Sample_Selection.R
+++ 5_Sample_Selection.R
@@ -18,11 +18,13 @@
 # Sample code for the problem sets in the course ECO 6935-6,
 # Capstone Project in Business Analytics, for the MSBA program.
 # Red_Fin_Reels_Selection_Models gives examples of
-#  sample selection models.
+#  sample selection models, using the Tobit model, type V.
 #
 # Dependencies:
 #   sampleSelection library to estimate models
 #     with sample selection.
+#   Note: This script reads the data and calculates variables, 
+#     so no other scripts need to be run first.
 #
 ##################################################
 
@@ -32,7 +34,7 @@
 ##################################################
 
 # Clear workspace.
-rm(list=ls(all=TRUE))
+# rm(list=ls(all=TRUE))
 
 # Set working directory, if running interactively.
 # wd_path <- '~/GitHub/Red_Fin_Reels/'
@@ -495,34 +497,35 @@ summary(tobit_5_sel_5)
 # from the probit model.
 # Now eliminate variables from the selection equation.
 
-tobit_5_sel_6 <-tobit_5_sel_5 <-
-  selection(selection = made_in_USA ~ 
-              # Width + 
-              # Diameter +
-              # Volume + 
-              # Weight + # Commented out.
-              # Density + # Commented out.
-              # Machined + 
-              Sealed,
-            outcome = list(log_Price_other ~ 
-                             Width + 
-                             # Diameter + # Commented out.
-                             # Volume + 
-                             # Weight +
-                             Density +
-                             Machined +
-                             Sealed,
-                           log_Price_USA ~ 
-                             # Width + # Commented out.
-                             Diameter +
-                             # Volume + 
-                             # Weight +
-                             Density +
-                             # Machined + 
-                             Sealed),
-            iterlim = 20,
-            # method = '2step',
-            data = flyreels)
+# Note: Commented out because it throws an error.
+# tobit_5_sel_6 <-tobit_5_sel_5 <-
+#   selection(selection = made_in_USA ~ 
+#               # Width + 
+#               # Diameter +
+#               # Volume + 
+#               # Weight + # Commented out.
+#               # Density + # Commented out.
+#               # Machined + 
+#               Sealed,
+#             outcome = list(log_Price_other ~ 
+#                              Width + 
+#                              # Diameter + # Commented out.
+#                              # Volume + 
+#                              # Weight +
+#                              Density +
+#                              Machined +
+#                              Sealed,
+#                            log_Price_USA ~ 
+#                              # Width + # Commented out.
+#                              Diameter +
+#                              # Volume + 
+#                              # Weight +
+#                              Density +
+#                              # Machined + 
+#                              Sealed),
+#             iterlim = 20,
+#             # method = '2step',
+#             data = flyreels)
 
 # Error in if (rho1 <= -1) rho1 <- -0.99 :
 #   missing value where TRUE/FALSE needed
@@ -606,6 +609,143 @@ summary(tobit_5_sel_5)
 # which measures a similar percentage difference.
 # No matter how it is measured, there exists a substantial premium
 # for fly reels made in the USA.
+
+
+##################################################
+# Making Predictions
+##################################################
+
+# Now use this model to make predictions.
+
+
+#--------------------------------------------------
+# Set Parameters
+#--------------------------------------------------
+
+
+shipping_Asia <- 65/12
+shipping_USA <- 5/12
+
+wage_Asia <- 7
+wage_USA <- 32
+
+# Weight same regardless of manufacture
+# Difference is detail.
+
+machining_hrs <- 6
+casting_hrs <- 5
+sealing_hrs <- 2
+
+# Price of aluminum
+price_alum <- 1.50
+
+
+
+
+# State parameters of our fly reel design.
+width_decision <- 1
+weight_decision <- 6
+diameter_decision <- 4
+density_decision <- weight_decision/(pi*(diameter_decision/2)^2*width_decision)
+
+
+#--------------------------------------------------
+# Enumerate the alternatives
+#--------------------------------------------------
+
+# Enumerate the possibilities for our fly reel
+# manufacturing decisions.
+design_options <- expand.grid(Machined = c('No', 'Yes'), 
+                              Sealed = c('No', 'Yes'), 
+                              made_in_USA = c(FALSE, TRUE))
+
+# Create a data frame listing the characteristics
+# of our fly reel design.
+num_options <- nrow(design_options)
+flyreel_decision <- data.frame(Weight = rep(weight_decision, num_options), 
+                               Width = rep(width_decision, num_options), 
+                               Diameter = rep(diameter_decision, num_options), 
+                               Density = rep(density_decision, num_options))
+# Append the variables representing our options
+# for manufacturing decisions.
+decision_vars <- colnames(design_options)
+flyreel_decision[, decision_vars] <- design_options
+
+#--------------------------------------------------
+# Calculate the costs under each alternative
+#--------------------------------------------------
+
+# Calculate cost of materials (same, regardless).
+flyreel_decision[, 'cost'] <- 
+  flyreel_decision[, 'Weight']*16*price_alum
+
+# Add cost of shipping (differs by country).
+flyreel_decision[, 'cost'] <- 
+  flyreel_decision[, 'cost'] + 
+  (flyreel_decision[, 'made_in_USA'] == TRUE)*shipping_USA + 
+  (flyreel_decision[, 'made_in_USA'] == FALSE)*shipping_Asia
+
+# Add cost of labor for machining, if used (wage differs by country).
+flyreel_decision[, 'cost'] <- 
+  flyreel_decision[, 'cost'] + 
+  ((flyreel_decision[, 'Machined'] == 'Yes')*machining_hrs) * 
+  ((flyreel_decision[, 'made_in_USA'] == TRUE)*wage_USA + 
+     (flyreel_decision[, 'made_in_USA'] == FALSE)*wage_Asia)
+
+# Add cost of labor for casting, if used, instead (wage differs by country).
+flyreel_decision[, 'cost'] <- 
+  flyreel_decision[, 'cost'] + 
+  ((flyreel_decision[, 'Machined'] == 'No')*casting_hrs) * 
+  ((flyreel_decision[, 'made_in_USA'] == TRUE)*wage_USA + 
+     (flyreel_decision[, 'made_in_USA'] == FALSE)*wage_Asia)
+
+# Add cost of labor for sealing, if performed (wage differs by country).
+flyreel_decision[, 'cost'] <- 
+  flyreel_decision[, 'cost'] + 
+  ((flyreel_decision[, 'Sealed'] == 'Yes')*sealing_hrs) * 
+  ((flyreel_decision[, 'made_in_USA'] == TRUE)*wage_USA + 
+     (flyreel_decision[, 'made_in_USA'] == FALSE)*wage_Asia)
+
+# Inspect the options on a cost basis.
+flyreel_decision
+
+#--------------------------------------------------
+# Calculate the predictions 
+#--------------------------------------------------
+
+
+# Calculate predictions.
+# Note that Tobit is different, since it gives two predictions.
+flyreel_decision[, 'pred_tobit'] <- NA
+sel_rows <- flyreel_decision[, 'made_in_USA'] == FALSE
+flyreel_decision[sel_rows, 'pred_tobit'] <- 
+  predict(tobit_5_sel_5, newdata = flyreel_decision)[sel_rows, 'E[yo1]']
+sel_rows <- flyreel_decision[, 'made_in_USA'] == TRUE
+flyreel_decision[sel_rows, 'pred_tobit'] <- 
+  predict(tobit_5_sel_5, newdata = flyreel_decision)[sel_rows, 'E[yo2]']
+
+
+
+# Calculate predicted sale price.
+flyreel_decision[, 'price_tobit'] <- exp(flyreel_decision[, 'pred_tobit'])
+
+
+
+#--------------------------------------------------
+# Calculate the profitability 
+#--------------------------------------------------
+
+# Calculate the profit earned on each unit.
+flyreel_decision[, 'profit_tobit'] <- flyreel_decision[, 'price_tobit'] - 
+  flyreel_decision[, 'cost']
+
+# Compare the options by profitability.
+flyreel_decision
+
+
+
+
+
 
 
 ##################################################
